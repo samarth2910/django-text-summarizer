3b87cca0b557511de843a9493e7db4827fd68354
samarth2910 <samarthshetty212@gmail.com>
2025-08-03 02:46:41 +0530

    Fix: Added CSRF trusted origins for Railway1

--- summarizer/summarizer/settings.py
+++ summarizer/summarizer/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-%lu_yqv2k5oshz$^6d=)noq2bu08&app^n6)yp3_#&5$cs0j-%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-text-summarizer-production.up.railway.app',
+    'https://*.railway.app'
+]
+
 
 
 
